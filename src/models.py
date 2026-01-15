5854fdeec0168ee172977bf775408b278901b605
Steven Acero <stevenacero07@gmail.com>
2026-01-15 03:55:30 +0000

    Agregar modelos de base de datos para Post, Media, Comment y Follower

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -17,3 +17,33 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(backref="posts")
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str]= mapped_column(String(120))
+    
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post : Mapped["Post"] = relationship(backref="comments")
+
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(backref="comments")
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+
+    user_to_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_to: Mapped["User"] =relationship(backref="followers")
+
+    user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_from: Mapped["User"] =relationship(backref="followings")
