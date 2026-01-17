00164ad3448a54f8c1646afd528bbaa387856ed6
Steven Acero <stevenacero07@gmail.com>
2026-01-17 01:56:31 +0000

    Agregar métodos de serialización para las clases Post, Media, Comment y Follower

--- src/models.py
+++ src/models.py
@@ -23,11 +23,26 @@ class Post(db.Model):
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user: Mapped["User"] = relationship(backref="posts")
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "user": self.user.serialize()
+        }
+
 class Media(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[str] = mapped_column(nullable=False)
     url: Mapped[str] = mapped_column(nullable=False)
     post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id
+        }
 
 class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -39,6 +54,14 @@ class Comment(db.Model):
     author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     author: Mapped["User"] = relationship(backref="comments")
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "author":self.author.serialize()
+        }
+
 class Follower(db.Model):
     id: Mapped[int] = mapped_column(primary_key = True)
 
@@ -47,3 +70,12 @@ class Follower(db.Model):
 
     user_from_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
     user_from: Mapped["User"] =relationship(backref="followings")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_to_id": self.user_to_id,
+            "user_to": self.user_to.serialize(),
+            "user_from_id":self.user_from_id,
+            "user_from": self.user_from.serialize()
+        }
